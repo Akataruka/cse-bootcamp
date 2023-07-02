f20d7c2360f20b0d4821b6f543f0ec32cdd228b7
Akataruka <121378361+Akataruka@users.noreply.github.com>
2023-07-02 15:36:02 +0530

    responsive video

--- home-page/home.js
+++ home-page/home.js
@@ -1,2 +1,19 @@
-let video_size=document.getElementById("vid")
-video_size.style.width=
+function video_width_responsive() {
+    let width = window.innerWidth;
+    let video = document.getElementsByClassName("video_animation");
+    if (width <= 1200 && width >= 1000) {
+        let new_width = (32 - 4 * (12 - (width / 100)));
+        video[0].style.top = `${new_width}vh`;
+    } else if (width >= 600 && width <= 1000) {
+        let new_width = (30 - 4 * (12 - (width / 100)));
+        video[0].style.top = `${new_width}vh`;
+    }
+    else if (width >= 300 && width <= 600) {
+        let new_width = (32 - 4 * (12 - (width / 100)));
+        video[0].style.top = `${new_width}vh`;
+    }
+    else {
+        video[0].style.top = `35vh`;
+    }
+}
+setInterval(video_width_responsive,1);

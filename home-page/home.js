b5ac81707655cfe87d3c583d729f5dd441ef28b3
Akataruka <121378361+Akataruka@users.noreply.github.com>
2023-07-04 20:57:15 +0530

    04/06/23

--- home-page/home.js
+++ home-page/home.js
@@ -17,3 +17,18 @@ function video_width_responsive() {
     }
 }
 setInterval(video_width_responsive,1);
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("#table");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+{
+    let width = window.innerWidth;
+    let body_width = document.getElementsByTagName("body");
+    body_width[0].style.width = width;
+}
--- landing-page/index.js
+++ landing-page/index.js
@@ -12,3 +12,20 @@ function mobileMenu() {
     let body_width = document.getElementsByTagName("body");
     body_width[0].style.width = width;
 }
+
+
+{
+    let width=window.innerWidth;
+    let cseimg2=document.getElementById("cseimg2");
+    if (width>=1500){
+        cseimg2.style.left="20em";
+    }
+    else if(width<1500 && width>=1330){
+        
+        cseimg2.style.left="14em";
+    }
+    else if(width<1330 && width>=1280){
+        
+        cseimg2.style.left="11em";
+    }
+}
